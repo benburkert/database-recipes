b917b23d65d7df4c961f35041bab94d7ad35f0cb
Ben Burkert <ben@benburkert.com>
2009-03-11 15:22:00 -0500

    Commenting out the package & unmask code for now.

--- cookbooks/postgresql/recipes/default.rb
+++ cookbooks/postgresql/recipes/default.rb
@@ -2,24 +2,24 @@
 # Cookbook Name:: postgresql
 # Recipe:: default
 #
-if node[:postgresql][:hard_masked]
-  execute "unmask-postgresql" do
-    command %Q{
-      echo "=dev-db/libpq-#{node[:postgresql][:version]}" >> /etc/portage/package.unmask
-      echo "=dev-db/postgresql-#{node[:postgresql][:version]}" >> /etc/portage/package.unmask
-    }
-
-    command %Q{
-      echo "=dev-db/libpq-#{node[:postgresql][:version]} ~amd64" >> /etc/portage/package.keywords/ec2
-      echo "=dev-db/postgresql-#{node[:postgresql][:version]} ~amd64" >> /etc/portage/package.keywords/ec2
-    }
-  end
-end
-
-package "postgresql" do
-  version node[:postgresql][:version]
-  action :install
-end
+#if node[:postgresql][:hard_masked]
+#  execute "unmask-postgresql" do
+#    command %Q{
+#      echo "=dev-db/libpq-#{node[:postgresql][:version]}" >> /etc/portage/package.unmask
+#      echo "=dev-db/postgresql-#{node[:postgresql][:version]}" >> /etc/portage/package.unmask
+#    }
+#
+#    command %Q{
+#      echo "=dev-db/libpq-#{node[:postgresql][:version]} ~amd64" >> /etc/portage/package.keywords/ec2
+#      echo "=dev-db/postgresql-#{node[:postgresql][:version]} ~amd64" >> /etc/portage/package.keywords/ec2
+#    }
+#  end
+#end
+#
+#package "postgresql" do
+#  version node[:postgresql][:version]
+#  action :install
+#end
 
 directory "/db/postgresql/data" do
   owner "postgresql"
