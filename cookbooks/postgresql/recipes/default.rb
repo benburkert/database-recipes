fea62b723cf28a9dc2c6d070d184a2744983e5b4
Ben Burkert <ben@benburkert.com>
2009-03-11 15:13:43 -0500

    Tweaked the unmask command.

--- cookbooks/postgresql/recipes/default.rb
+++ cookbooks/postgresql/recipes/default.rb
@@ -5,8 +5,13 @@
 if node[:postgresql][:hard_masked]
   execute "unmask-postgresql" do
     command %Q{
-      mkdir -p /etc/portage
+      echo "=dev-db/libpq-#{node[:postgresql][:version]}" >> /etc/portage/package.unmask
       echo "=dev-db/postgresql-#{node[:postgresql][:version]}" >> /etc/portage/package.unmask
+    }
+
+    command %Q{
+      echo "=dev-db/libpq-#{node[:postgresql][:version]} ~amd64" >> /etc/portage/package.keywords/ec2
+      echo "=dev-db/postgresql-#{node[:postgresql][:version]} ~amd64" >> /etc/portage/package.keywords/ec2
     }
   end
 end
