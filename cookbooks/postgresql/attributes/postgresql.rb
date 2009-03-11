707c1c3a7aa5278e0afb9c152031ba5af06508a9
Ben Burkert <ben@benburkert.com>
2009-03-11 15:00:58 -0500

    Commented out the instance_type code for now.

--- cookbooks/postgresql/attributes/postgresql.rb
+++ cookbooks/postgresql/attributes/postgresql.rb
@@ -4,7 +4,7 @@ postgresql Mash.new unless attribute?('postgresql')
 postgresql[:version]    = "8.3.1"
 postgresql[:hard_masked]  = true
 
-case instance_type
-when 'm1.large'
-  postgresql[:buffer] = 100
-end
+#case instance_type
+#when 'm1.large'
+#  postgresql[:buffer] = 100
+#end
