465068200d1ed3ed172b95018e22828229e78de6
Ben Burkert <ben@benburkert.com>
2009-03-11 15:02:59 -0500

    Using the 'node' method to access the postgres attributes.

--- cookbooks/postgresql/recipes/default.rb
+++ cookbooks/postgresql/recipes/default.rb
@@ -2,17 +2,17 @@
 # Cookbook Name:: postgresql
 # Recipe:: default
 #
-if postgresql[:hard_masked]
+if node[:postgresql][:hard_masked]
   execute "unmask-postgresql" do
     command %Q{
       mkdir -p /etc/portage
-      echo "=dev-db/postgresql-#{postgresql[:version]}" >> /etc/portage/package.unmask
+      echo "=dev-db/postgresql-#{node[:postgresql][:version]}" >> /etc/portage/package.unmask
     }
   end
 end
 
 package "postgresql" do
-  version postgresql[:version]
+  version node[:postgresql][:version]
   action :install
 end
 
