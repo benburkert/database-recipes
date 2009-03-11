8b4a9118be0b068c8a7036a5a3b7f5125cbaee79
Ben Burkert <ben@benburkert.com>
2009-03-11 16:09:57 -0500

    Removed the postgres attributes, added a conf.d/postgresql template.

--- cookbooks/postgresql/recipes/default.rb
+++ cookbooks/postgresql/recipes/default.rb
@@ -2,28 +2,20 @@
 # Cookbook Name:: postgresql
 # Recipe:: default
 #
-#if node[:postgresql][:hard_masked]
-#  execute "unmask-postgresql" do
-#    command %Q{
-#      echo "=dev-db/libpq-#{node[:postgresql][:version]}" >> /etc/portage/package.unmask
-#      echo "=dev-db/postgresql-#{node[:postgresql][:version]}" >> /etc/portage/package.unmask
-#    }
-#
-#    command %Q{
-#      echo "=dev-db/libpq-#{node[:postgresql][:version]} ~amd64" >> /etc/portage/package.keywords/ec2
-#      echo "=dev-db/postgresql-#{node[:postgresql][:version]} ~amd64" >> /etc/portage/package.keywords/ec2
-#    }
-#  end
-#end
-#
-#package "postgresql" do
-#  version node[:postgresql][:version]
-#  action :install
-#end
 
 directory "/db/postgresql/data" do
   owner "postgresql"
   group "postgresql"
   mode 0755
   recursive true
+end
+
+template "/etc/conf.d/postgresql" do
+  owner 'root'
+  group 'root'
+  mode 0644
+  source "postgresql.erb"
+  variables :basedir  => '/db/postgresql',
+            :user     => 'postgres',
+            :group    => 'postgres'
 end
