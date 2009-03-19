fdeebc6624db563870d463fac2bfc3aea7aa591f
Ben Burkert <ben@benburkert.com>
2009-03-19 15:21:12 -0500

    Moved the postgres config template, Added the initdb command.

--- cookbooks/postgresql/recipes/default.rb
+++ cookbooks/postgresql/recipes/default.rb
@@ -10,12 +10,19 @@ directory "/db/postgresql/data" do
   recursive true
 end
 
-template "/etc/conf.d/postgresql" do
+template "/etc/conf.d/postgresql-8.3" do
   owner 'root'
   group 'root'
   mode 0644
-  source "postgresql.erb"
+  source "postgresql-8.3.erb"
   variables :basedir  => '/db/postgresql',
             :user     => 'postgres',
             :group    => 'postgres'
+end
+
+execute 'init-postgres-database' do
+  command %Q{
+    su - postgres -c "initdb --locale=en_US.UTF-8 -E=UNICODE /db/postgresql/data"
+  }
+  if_not "ls /db/postgresql/data"
 end
