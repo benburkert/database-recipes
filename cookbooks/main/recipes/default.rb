44e7e4d7fab18036ea90ae4a7bc6e26483acf9df
Ben Burkert <ben@benburkert.com>
2009-03-19 16:08:38 -0500

    Merging in the latest engineyard changes.

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -6,4 +6,8 @@ end
 
 # uncomment if you want to run couchdb recipe
 # require_recipe "couchdb"
+
+# uncomment to turn your instance into an integrity CI server
+#require_recipe "integrity"
+
 require_recipe "postgresql"
