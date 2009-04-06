a63ce50257e88bb91540dc26a7d2cae362fb88ff
Ben Burkert <ben@benburkert.com>
2009-04-05 21:12:44 -0500

    Non DB instances shouldn't define the postgresql attributes.

--- cookbooks/postgresql/attributes/postgresql.rb
+++ cookbooks/postgresql/attributes/postgresql.rb
@@ -16,26 +16,29 @@ when 'm1.xlarge'
   mbs   = 15360.13672
 end
 
-postgresql['postgresql.conf'] ||= Mash.new
-postgresql['postgresql.conf'][:listen_addresses]  = '*' #ifconfig lists nothing about the ipv4 address
-postgresql['postgresql.conf'][:port]              = 5432
-
-# Optimize'able stuff
-postgresql[:shared_buffer_space]                      = (bytes * 0.11).to_i #11% of the total RAM
-postgresql['postgresql.conf'][:shared_buffers]        = (mbs * 0.10).to_i   #91% of the buffer space
-postgresql['postgresql.conf'][:effective_cache_size]  = (mbs * 0.75).to_i   #75% of the total RAM
-
-postgresql['postgresql.conf'][:maintenance_work_mem]  = 384 #384MB
-postgresql['postgresql.conf'][:work_mem]              = 64  #64MB
-postgresql['postgresql.conf'][:temp_buffers]          = 64  #64MB
-
-postgresql['postgresql.conf'][:checkpoint_segments]       = 128
-postgresql['postgresql.conf'][:default_statistics_target] = 100
-
-postgresql['postgresql.conf'][:vacuum_cost_delay]    = 200
-postgresql['postgresql.conf'][:random_page_cost]     = "3.0"
-
-postgresql['postgresql.conf'][:autovacuum_vacuum_threshold]      = 400
-postgresql['postgresql.conf'][:autovacuum_vacuum_scale_factor]   = "0.05"
-postgresql['postgresql.conf'][:autovacuum_analyze_threshold]     = 200
-postgresql['postgresql.conf'][:autovacuum_analyze_scale_factor]  = "0.01"
+case instance_type
+when 'm1.large', 'm1.xlarge'
+  postgresql['postgresql.conf'] ||= Mash.new
+  postgresql['postgresql.conf'][:listen_addresses]  = '*' #ifconfig lists nothing about the ipv4 address
+  postgresql['postgresql.conf'][:port]              = 5432
+  
+  # Optimize'able stuff
+  postgresql[:shared_buffer_space]                      = (bytes * 0.11).to_i #11% of the total RAM
+  postgresql['postgresql.conf'][:shared_buffers]        = (mbs * 0.10).to_i   #91% of the buffer space
+  postgresql['postgresql.conf'][:effective_cache_size]  = (mbs * 0.75).to_i   #75% of the total RAM
+  
+  postgresql['postgresql.conf'][:maintenance_work_mem]  = 384 #384MB
+  postgresql['postgresql.conf'][:work_mem]              = 64  #64MB
+  postgresql['postgresql.conf'][:temp_buffers]          = 64  #64MB
+  
+  postgresql['postgresql.conf'][:checkpoint_segments]       = 128
+  postgresql['postgresql.conf'][:default_statistics_target] = 100
+  
+  postgresql['postgresql.conf'][:vacuum_cost_delay]    = 200
+  postgresql['postgresql.conf'][:random_page_cost]     = "3.0"
+  
+  postgresql['postgresql.conf'][:autovacuum_vacuum_threshold]      = 400
+  postgresql['postgresql.conf'][:autovacuum_vacuum_scale_factor]   = "0.05"
+  postgresql['postgresql.conf'][:autovacuum_analyze_threshold]     = 200
+  postgresql['postgresql.conf'][:autovacuum_analyze_scale_factor]  = "0.01"
+end
