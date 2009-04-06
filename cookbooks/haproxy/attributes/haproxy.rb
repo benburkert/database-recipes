ce976326cb9d56d0b710d282ada453e69f617b11
Ben Burkert <ben@benburkert.com>
2009-04-05 23:05:40 -0500

    More bugs.

--- cookbooks/haproxy/attributes/haproxy.rb
+++ cookbooks/haproxy/attributes/haproxy.rb
@@ -4,5 +4,5 @@ haproxy[:user]        = @attribute[:user]
 haproxy[:password]    = @attribute[:password]
 haproxy[:application] = @attribute[:application]
 
-haproxy[:upstream_port] = @attribute[:upstream_port]
-haproxy[:ports]         = @attribute[:ports]
+haproxy[:upstream_port] = @attribute[:upstream_port] || 5000
+haproxy[:ports]         = @attribute[:ports] || 5001..5005
